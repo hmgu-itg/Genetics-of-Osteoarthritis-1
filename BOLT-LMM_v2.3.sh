27704708a086c103ec875e8808aab464f09c7ef0
Konstantinos Hatzikotoulas <hatzikotoulas@helmholtz-muenchen.de>
2021-09-03 12:28:05 +0200

    Update BOLT-LMM_v2.3.sh

--- BOLT-LMM_v2.3.sh
+++ BOLT-LMM_v2.3.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/BOLT-LMM_v2.3/bolt \
-        --bed=/lustre/scratch115/projects/ukbiobank/FullRelease/Genotypes/001/ukb_cal_chr{1:22}_v2.bed \
-        --bim=/lustre/scratch115/projects/ukbiobank/FullRelease/Genotypes/001/ukb_snp_chr{1:22}_v2.bim \
-        --fam=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/VERSION_3/HD_hipknee/ukb9979_cal_chr1_v2_s488363_HD_hipknee_FixCol6.fam \
-        --remove=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/VERSION_3/SR/bolt.in_plink_but_not_imputed.FID_IID.968.txt \
-        --exclude=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/VERSION_3/SR/autosome_maf_lt_0.001.txt \
-		--exclude=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/VERSION_3/SR/autosome_missing_gt_0.1.txt \
-        --phenoFile=/lustre/scratch115/projects/ukbiobank_oa/OA_sample-file_v3_12042018/HD_hipknee_ukb9979_imp_chr1_v3_s487395_pheno-covar.sample \
+# PROGRAM PATHS
+bolt=$1
+# DATA PATHS
+Geno_bed=$2
+Geno_bim=$3
+Geno_fam=$4
+pheno_cov=$5
+sample=$6
+
+$bolt \
+        --bed=$Geno_bed \
+        --bim=$Geno_bim \
+        --fam=$Geno_fam \
+        --remove=bolt.in_plink_but_not_imputed.FID_IID.968.txt \
+        --exclude=autosome_maf_lt_0.001.txt \
+	--exclude=autosome_missing_gt_0.1.txt \
+        --phenoFile=$pheno_cov \
         --phenoCol=OA_phenotype \
-        --covarFile=/lustre/scratch115/projects/ukbiobank_oa/OA_sample-file_v3_12042018/HD_hipknee_ukb9979_imp_chr1_v3_s487395_pheno-covar.sample \
+        --covarFile=$pheno_cov \
         --qCovarCol=PC{1:20} \
-        --LDscoresFile=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/BOLT-LMM_v2.3/tables/LDSCORE.1000G_EUR.tab.gz \
+        --LDscoresFile=LDSCORE.1000G_EUR.tab.gz \
         --LDscoresMatchBp \
-        --geneticMapFile=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/BOLT-LMM_v2.3/tables/genetic_map_hg19_withX.txt.gz \
+        --geneticMapFile=genetic_map_hg19_withX.txt.gz \
         --lmmForceNonInf \
         --numThreads=8 \
-        --statsFile=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/VERSION_3/HD_hipknee/chr${LSB_JOBINDEX}_bolt_HD_hipknee-OA.stats.gz \
-        --bgenFile=/lustre/scratch115/projects/ukbiobank/FullRelease/Imputed/EGAD00010001474/ukb_imp_chr${LSB_JOBINDEX}_v3.bgen \
+        --statsFile=chr${LSB_JOBINDEX}_bolt_HD_hipknee-OA.stats.gz \
+        --bgenFile=ukb_imp_chr${LSB_JOBINDEX}_v3.bgen \
         --bgenMinINFO=0.3 \
-        --sampleFile=/lustre/scratch115/projects/ukbiobank_oa/OA_sample-file_v3_12042018/HD_hipknee_ukb9979_imp_chr1_v3_s487395.sample \
-        --statsFileBgenSnps=/lustre/scratch115/projects/ukbiobank/Kostas/FULL_RELEASE/BOLT-LMM_v2.3/VERSION_3/HD_hipknee/chr${LSB_JOBINDEX}_bolt_HD_hipknee-OA.bgen.stats.gz \
+        --sampleFile=$sample \
+        --statsFileBgenSnps=chr${LSB_JOBINDEX}_bolt_HD_hipknee-OA.bgen.stats.gz \
         --verboseStats
